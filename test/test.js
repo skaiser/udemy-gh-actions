c548dce61664afc50d928cd24fc500f2d1ad968a
Stephen Kaiser <freesol29@gmail.com>
2022-10-21 23:29:14 -0700

    Run prettier on test file

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
-const assert = require('assert');
-describe('Simple tests', () => {
-  it('sums to 2', () => {
+const assert = require("assert");
+describe("Simple tests", () => {
+  it("sums to 2", () => {
     assert.equal(1 + 1, 2);
   });
-  it('product is 9', () => {
+  it("product is 9", () => {
     assert.equal(3 * 3, 9);
   });
-  it('returns 1', () => {
+  it("returns 1", () => {
     assert.equal(6 / 6, 1);
   });
 });
